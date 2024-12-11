4f90158e09300d6affe9dd22706a363a770aaef4
Supper Thomas <78900636@qq.com>
2024-12-11 20:22:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import json
 import os
+import re
 
 def extract_source_dirs(compile_commands):
     source_dirs = set()
 
     for entry in compile_commands:
-        # 获取文件路径
         file_path = entry['file']
 
-        # 确认是 .c 文件
         if file_path.endswith('.c'):
-            # 获取文件所在目录
             dir_path = os.path.dirname(os.path.abspath(file_path))
             source_dirs.add(dir_path)
 
@@ -31,41 +29,59 @@ def extract_source_dirs(compile_commands):
 
     return sorted(source_dirs)
 
-def generate_code_workspace_file(source_dirs, workspace_name='project_workspace'):
-    # 获取当前工作目录以计算相对路径
-    current_working_directory = os.getcwd()
+def consolidate_directories(dirs):
+    consolidated = set()
+    dirs = sorted(dirs)
 
-    # 将所有源目录转换为相对路径
+    pattern = re.compile(r"^(.*?\\[a-zA-Z][^\\]*)")
+
+    for dir_path in dirs:
+        if dir_path == '.':
+            consolidated.add('.')
+        elif dir_path.startswith('..'):
+            match = pattern.match(dir_path)
+            if match:
+                reduced_dir = match.group(1)
+                #print(reduced_dir)
+                consolidated.add(reduced_dir)
+
+    return sorted(consolidated)
+
+def generate_code_workspace_file(source_dirs):
+
+    current_working_directory = os.getcwd()
+    current_folder_name = os.path.basename(current_working_directory)
+    
     relative_dirs = []
     for dir_path in source_dirs:
         try:
             rel_path = os.path.relpath(dir_path, start=current_working_directory)
-            if rel_path.startswith(('.', '..')):
-                relative_dirs.append(rel_path)
+            relative_dirs.append(rel_path)
         except ValueError:
-            # 忽略不同驱动器上的路径
             continue
-            
+    
+    relative_dirs.sort(key=lambda x: (x != '.', x))
+    source_dirs = consolidate_directories(relative_dirs)
+    
     workspace_data = {
-        "folders": [{"path": dir_path} for dir_path in relative_dirs],
+        "folders": [{"path": dir_path} for dir_path in source_dirs],
         "settings": {"clangd.arguments": [
                 "--compile-commands-dir=.",
                 "--header-insertion=never"
             ]}
     }
-    
-    with open(f'{workspace_name}.code-workspace', 'w') as f:
+
+    workspace_filename = f'{current_folder_name}.code-workspace'
+    with open(workspace_filename, 'w') as f:
         json.dump(workspace_data, f, indent=4)
 
+    print(f'Workspace file {workspace_filename} created.')
+
 def main():
-    # 读取 compile_commands.json
     with open('compile_commands.json', 'r') as f:
         compile_commands = json.load(f)
     
-    # 提取源文件目录
     source_dirs = extract_source_dirs(compile_commands)
-    
-    # 生成和保存工作空间文件
     generate_code_workspace_file(source_dirs)
 
 if __name__ == '__main__':
