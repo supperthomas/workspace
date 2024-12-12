97c0ff768c934e27bcd91da140d24b27caee795b
Supper Thomas <78900636@qq.com>
2024-12-12 15:54:22 +0800

    Update version2.py

--- version2.py
+++ version2.py
@@ -1,6 +1,7 @@
 import os
 import json
 import argparse
+
 def find_first_node_with_two_children(tree):
     for key, subtree in tree.items():
         if len(subtree) >= 2:
@@ -10,11 +11,13 @@ def find_first_node_with_two_children(tree):
             return result
     return None, None
 
+
 def filt_tree(tree):
     key, subtree = find_first_node_with_two_children(tree)
     if key:
         return {key: subtree}
     return {}
+
 
 def add_path_to_tree(tree, path):
     parts = path.split(os.sep)
@@ -24,17 +27,32 @@ def add_path_to_tree(tree, path):
             current_level[part] = {}
         current_level = current_level[part]
 
+
 def build_tree(paths):
     tree = {}
+    current_working_directory = os.getcwd()
+    current_folder_name = os.path.basename(current_working_directory)
+    #过滤异常路径
+    relative_dirs = []
+    for dir_path in paths:
+        try:
+            rel_path = os.path.relpath(dir_path, start=current_working_directory)
+            relative_dirs.append(rel_path)
+        except ValueError:
+            print(f"delete dir_path:{dir_path}")
+            paths.delete(dir_path)
+            continue
     for path in paths:
         normalized_path = os.path.normpath(path)
         add_path_to_tree(tree, normalized_path)
     return tree
 
+
 def print_tree(tree, indent=''):
     for key, subtree in sorted(tree.items()):
         print(indent + key)
         print_tree(subtree, indent + '  ')
+
 
 def extract_source_dirs(compile_commands):
     source_dirs = set()
@@ -47,7 +65,7 @@ def extract_source_dirs(compile_commands):
             source_dirs.add(dir_path)
             # command 或者arguments
             command = entry.get('command') or entry.get('arguments')
-            
+
             if isinstance(command, str):
                 parts = command.split()
             else:
@@ -60,51 +78,83 @@ def extract_source_dirs(compile_commands):
                 elif part.startswith('/I'):
                     include_dir = part[2:] if len(part) > 2 else parts[i + 1]
                     source_dirs.add(os.path.abspath(include_dir))
-
+    print(f"Source Directories: {source_dirs}")
     return sorted(source_dirs)
+
 
 def is_path_in_tree(path, tree):
     parts = path.split(os.sep)
     current_level = tree
     found_first_node = False
     root_key = list(tree.keys())[0]
-    for part in parts:
-        if not found_first_node:
-            print(part)
-            print(root_key)
-            if part == root_key:
-                found_first_node = True
-            else:
-                continue
-        else:
-            if part in current_level:
-                current_level = current_level[part]
-            else:
-                return False
-    if found_first_node == False:
+    print(root_key)
+    print(path)
+    index_start = parts.index(root_key)
+    length = len(parts)
+    try:
+        for i in range(index_start, length):
+            current_level = current_level[parts[i]]
+        return True
+    except KeyError:
         return False
-    return True
-    
+
+
+def generate_code_workspace_file(source_dirs):
+    current_working_directory = os.getcwd()
+    current_folder_name = os.path.basename(current_working_directory)
+
+    relative_dirs = []
+    for dir_path in source_dirs:
+        try:
+            rel_path = os.path.relpath(dir_path, root_path)
+            relative_dirs.append(rel_path)
+        except ValueError:
+            continue
+
+    root_rel_path = os.path.relpath(root_path, current_working_directory)
+    workspace_data = {
+        "folders": [
+            {
+                "path": f"{root_rel_path}"
+            }
+        ],
+        "settings": {
+            "clangd.arguments": [
+                "--compile-commands-dir=.",
+                "--header-insertion=never"
+            ],
+            "files.exclude": {dir: True for dir in relative_dirs}
+        }
+    }
+    workspace_filename = f'{current_folder_name}.code-workspace'
+    # print(workspace_data)
+    with open(workspace_filename, 'w') as f:
+        json.dump(workspace_data, f, indent=4)
+
+    print(f'Workspace file {workspace_filename} created.')
+
+
 def main(root_path):
     with open('compile_commands.json', 'r') as f:
         compile_commands = json.load(f)
-    
+
     source_dirs = extract_source_dirs(compile_commands)
     tree = build_tree(source_dirs)
+    print(tree)
     filtered_tree = filt_tree(tree)
     print("Filtered Directory Tree:")
     print_tree(filtered_tree)
 
     # 打印filtered_tree的root节点的相对路径
     root_key = list(filtered_tree.keys())[0]
-    print(f"Root node relative path: {root_path}")
+    print(f"Root node relative path: {root_key}")
 
     # 初始化exclude_fold集合
     exclude_fold = set()
 
-    os.chdir(root_path)
+    # os.chdir(root_path)
     # 轮询root文件夹下面的每一个文件夹和子文件夹
-    for root, dirs, files in os.walk('.'):
+    for root, dirs, files in os.walk(root_path):
         # 检查当前root是否在filtered_tree中
         if not is_path_in_tree(root, filtered_tree):
             exclude_fold.add(root)
@@ -117,10 +167,15 @@ def main(root_path):
 
     print("Excluded Folders:")
     print(exclude_fold)
+    generate_code_workspace_file(exclude_fold)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some paths.')
     parser.add_argument('root_path', type=str, help='The root path to start from')
     args = parser.parse_args()
-    main(args.root_path)
+    root_path = args.root_path
+    root_path_abs = os.path.abspath(root_path)
+    print(root_path_abs)
+    main(root_path_abs)
+
